1293ef2949d5e0a2dbf0af5784dc0b3336ce7248
wangyueren1 <wangyueren1@hotamil.com>
2017-03-28 21:19:35 -0700

    graphnode

---
+++ TreeAndGraphCommonLib/graphnode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeAndGraphCommonLib
+{
+    class graphnode
+    {
+        public int value { get; set; }
+        public List<graphnode> children { get; set; }
+        public graphnode()
+        {
+
+        }
+    }
+}
