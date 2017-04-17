0d3ad3c8086aa4846a651f49f32ebc4ffa47f42c
Yueren Wang <wangyueren1@hotmail.com>
2017-04-17 16:48:51 -0700

    robotinmaze

---
+++ RobotInMaze/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotInMaze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class coordinate
+    {
+        int x { get; set; }
+        int y { get; set; }
+    }
+}
+ 
